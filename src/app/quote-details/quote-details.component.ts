0b2ac69038675dd9e2f08bbe491d593d4f43d819
EKibet <kibetedgar@gmail.com>
2018-08-13 11:04:55 +0300

    Adds delete functionality

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,13 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Quotes} from '../quotes';
 
 @Component({
-  selector: 'app-quote-details',
-  templateUrl: './quote-details.component.html',
-  styleUrls: ['./quote-details.component.css']
+  selector: 'app-quotes-details',
+  templateUrl: './quotes-details.component.html',
+  styleUrls: ['./quotes-details.component.css']
 })
-export class QuoteDetailsComponent implements OnInit {
+export class QuotesDetailsComponent implements OnInit {
   @Input() quotes = Array<Quotes>();
+  confirmDelete: boolean;
+  templist: Array<Quotes>;
 
+  Confirm(){
+    this.confirmDelete = confirm("Are you sure you want to delete this quote?");
+  }
+  deleteQuote(index: number) {
+    this.Confirm();
+    if (this.confirmDelete) {
+      this.quotes.splice(index,1);
+    }
+    this.confirmDelete = false;
+  }
   constructor() { }
 
   ngOnInit() {
