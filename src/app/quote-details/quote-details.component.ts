3e7da6feaa9df656b7b2248c9050b3cc4a8cc316
EKibet <kibetedgar@gmail.com>
2018-08-13 11:05:16 +0300

    Adds highlight functionality

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -10,7 +10,15 @@ export class QuotesDetailsComponent implements OnInit {
   @Input() quotes = Array<Quotes>();
   confirmDelete: boolean;
   templist: Array<Quotes>;
+  highestVotedQuote(id) {
+    this.templist = this.quotes.slice();
+    this.templist.sort((a,b) => b.average() - a.average());
+    if (this.templist[0].id == id && this.templist[0].average() > 0) {
+      return true;
+    }
+    return false;
 
+  }
   Confirm(){
     this.confirmDelete = confirm("Are you sure you want to delete this quote?");
   }
