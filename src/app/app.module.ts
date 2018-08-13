fbb2133197dfddf36953a3ef659b7d3233926798
EKibet <kibetedgar@gmail.com>
2018-08-13 07:36:31 +0300

    initial commit of required components and landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NewQuoteComponent } from './new-quote/new-quote.component';
+import { NewDetailsComponent } from './new-details/new-details.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewQuoteComponent,
+    NewDetailsComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule
