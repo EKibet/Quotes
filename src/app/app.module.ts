3e9cb0d2691faa9a1d7938e6ff3537560342af05
EKibet <kibetedgar@gmail.com>
2018-08-13 11:51:38 +0300

    adds highlight directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { NewQuoteComponent } from './new-quote/new-quote.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { FormsModule } from "@angular/forms";
 import { QuotesDetailsComponent } from './quote-details/quote-details.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewQuoteComponent,
     LandingPageComponent,
-    QuotesDetailsComponent
+    QuotesDetailsComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
